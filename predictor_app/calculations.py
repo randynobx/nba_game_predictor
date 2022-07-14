ae3b631cccb4f6ec8a9542d15d9c9b2a4f23139f
Randy Nance <randynobx@gmail.com>
2022-07-14 10:01:19 -0400

    addional documentation for calculations.py

--- predictor_app/calculations.py
+++ predictor_app/calculations.py
@@ -3,39 +3,66 @@
 import pickle as pk
 import pandas as pd
 
-def calc_fourfactors(df: dict) -> dict:
-    '''Calculate four factors for each team'''
-    X = {}
-    h_dreb = df['h_trb'] - df['h_orb']
-    a_dreb = df['a_trb'] - df['a_orb']
+def calc_fourfactors(boxscore: dict) -> dict:
+    '''Calculate four factors for each team
+
+    Args:
+        boxscore (dict): dictionary of relevant boxscore data. Requires the following
+        keys: ['_fgm', '_3pm', '_fga', '_tov', '_ftm', '_fta', '_trb', '_orb']
+        prefixed with 'h' and 'a' for home and away teams respectively.
+
+    Returns:
+        four factors (dict): dictionary of four factors values calculated from
+        given boxscore
+    '''
+    fourfactors = {}
+    h_dreb = boxscore['h_trb'] - boxscore['h_orb']
+    a_dreb = boxscore['a_trb'] - boxscore['a_orb']
 
     # Shooting : (FG + 0.5 * 3FG) / FGA
-    X['h_efg'] = (df['h_fgm'] + 0.5 * df['h_3pm']) / df['h_fga']
-    X['a_efg'] = (df['a_fgm'] + 0.5 * df['a_3pm']) / df['a_fga']
+    fourfactors['h_efg'] = (boxscore['h_fgm'] + 0.5 * boxscore['h_3pm'])\
+                            / boxscore['h_fga']
+    fourfactors['a_efg'] = (boxscore['a_fgm'] + 0.5 * boxscore['a_3pm'])\
+                            / boxscore['a_fga']
 
     # Turnovers : TOV / (FGA + 0.44 * FTA + TOV)
-    X['h_tov'] = df['h_tov'] / (df['h_fga'] + 0.44 * df['h_fta'] + df['h_tov'])
-    X['a_tov'] = df['a_tov'] / (df['a_fga'] + 0.44 * df['a_fta'] + df['h_tov'])
+    fourfactors['h_tov'] = boxscore['h_tov'] / (boxscore['h_fga']
+                                                + 0.44 * boxscore['h_fta']
+                                                + boxscore['h_tov'])
+    fourfactors['a_tov'] = boxscore['a_tov'] / (boxscore['a_fga']
+                                                + 0.44 * boxscore['a_fta']
+                                                + boxscore['h_tov'])
 
     # Rebounding : DRB / (Opp ORB + DRB)
-    X['h_drb'] = h_dreb / (df['a_orb'] + h_dreb)
-    X['a_drb'] = a_dreb / (df['h_orb'] + a_dreb)
+    fourfactors['h_drb'] = h_dreb / (boxscore['a_orb'] + h_dreb)
+    fourfactors['a_drb'] = a_dreb / (boxscore['h_orb'] + a_dreb)
 
     # Freethrows : FT / FGA
-    X['h_ftr'] = df['h_ftm'] / df['h_fga']
-    X['a_ftr'] = df['a_ftm'] / df['a_fga']
+    fourfactors['h_ftr'] = boxscore['h_ftm'] / boxscore['h_fga']
+    fourfactors['a_ftr'] = boxscore['a_ftm'] / boxscore['a_fga']
 
-    return X
+    return fourfactors
 
-def predict_winner(X: dict) -> tuple:
-    '''Return winner of game and probability of expected result'''
+def predict_winner(fourfactors_dict: dict) -> tuple:
+    '''Return winner of game and probability of expected result
+
+    Args:
+        fourfactors_dict (dict): dictionary of four factors values (floats), with the
+        following keys: ['h_efg', 'a_efg', 'h_tov', 'a_tov', 'h_drb', 'a_drb',
+        'h_ftr', 'a_ftr']
+
+    Returns:
+        winner, proba (str, float): Tuple of the projected winner and model's
+        probability of predicted outcome occuring.
+    '''
     # load model
-    with open('model.pickle', 'rb') as f:
-        model = pk.load(f)
+    with open('model.pickle', 'rb') as file:
+        model = pk.load(file)
+
     # predict
-    X = pd.DataFrame(X, index=[0]).astype(float)
-    result =  model.predict(X)[0]
-    proba = round(model.predict_proba(X).max(), 3)
+    fourfactors_df = pd.DataFrame(fourfactors_dict, index=[0]).astype(float)
+    result =  model.predict(fourfactors_df)[0]
+    proba = round(model.predict_proba(fourfactors_df).max(), 3)
     if result == 1:
         return 'Home team', proba
     else:
